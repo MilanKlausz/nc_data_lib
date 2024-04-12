599b12c35783a37901c778b31fae3a4c1268ab9d
MilanKlausz <Milan.Klausz@ess.eu>
2024-04-12 17:46:41 +0200

    initial store db checksum, updated db if checksum differs from stored

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import PouchDB from 'pouchdb';
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
+async function generateChecksum(fileUrl) {
+  // Fetch the file
+  const response = await fetch(fileUrl);
+  const fileContent = await response.text();
+
+  // Encode the file content as UTF-8
+  const encoder = new TextEncoder();
+  const dataBuffer = encoder.encode(fileContent);
+
+  // Generate the SHA-256 hash
+  const hashBuffer = await window.crypto.subtle.digest('SHA-256', dataBuffer);
+
+  // Convert the hash to a hexadecimal string
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+  return hashHex;
+ }
+
 document.addEventListener('alpine:init', () => {
   // Define the store
   Alpine.store('db', {
@@ -11,25 +30,72 @@ document.addEventListener('alpine:init', () => {
     init() {
       this._db = new PouchDB('ncrystal_db');
       this._db.info().then((result) => {
-        if (result.doc_count === 0) {
-          // If empty, fetch and populate the database
-          return fetch('autogen_db/db.json')
-            .then(response => response.json())
-            .then(data => this._db.bulkDocs(data.map((material, index) => ({ _id: index.toString(), ...material }))))
-            .then(() => this._db.allDocs({ include_docs: true }));
-        } else {
-          return this._db.allDocs({ include_docs: true });
+        if (result.doc_count === 0) { // If empty, fetch and populate the database
+          return this.populateDatabase();
+        } else { // Check if the database needs to be updated
+          console.log("Database exist, checking if it needs to be updated.");
+          return this.checkAndUpdateDatabase();
         }
       }).catch(function (err) {
         console.error(err);
       });
     },
-    getAll() {
-      return this._db.allDocs({ include_docs: true });
+    populateDatabase() {
+      // Fetch the db.json file and its checksum
+      return fetch('autogen_db/db.json')
+        .then(response => response.json())
+        .then(data => {
+          // Generate the checksum for the fetched db.json
+          return generateChecksum('autogen_db/db.json')
+          .then(checksum => {
+            console.log("new checksum is generated:", checksum);
+            // Store the checksum in the database
+            return this._db.put({ _id: 'versionInfo', checksum: checksum })
+            .then(() => {
+              // Populate the database
+              console.log("Populate database");
+              return this._db.bulkDocs(data.map((material, index) => ({ _id: index.toString(), ...material })))
+                .then(() => this._db.allDocs({ include_docs: true }));
+            });
+          });
+        });
+    },
+    checkAndUpdateDatabase() {
+      // Fetch the versionInfo document
+      return this._db.get('versionInfo').then(versionInfo => {
+        // Generate the current checksum for the db.json file
+        return generateChecksum('autogen_db/db.json').then(currentChecksum => {
+          // Compare the stored checksum with the current checksum
+          if (versionInfo.checksum !== currentChecksum) {
+            console.log("different checksum, clearing the db and repopulating it");
+            // If different, clear the database and repopulate
+            return this._db.destroy().then(() => {
+              this._db = new PouchDB('ncrystal_db');
+              return this.populateDatabase();
+            });
+          } else {
+            console.log("checksum is the same, no need to update")
+            // If the same, just fetch all documents
+            return this._db.allDocs({ include_docs: true });
+          }
+        });
+      }).catch(err => {
+        if (err.name === 'not_found') {
+          // If versionInfo document does not exist, populate the database
+          return this.populateDatabase();
+        } else {
+          console.error(err);
+        }
+      });
+    },
+    async getAll() {
+      return await this._db.allDocs({ include_docs: true }).then((result) => {
+        return result.rows.map(row => row.doc).filter(el => 'safekey' in el); //exclude the versionInfo document storing the db checksum
+      });
     },
     async getBySafeKey(safeKey) {
       return await this._db.allDocs({ include_docs: true }).then((result) => { //TODO refactor to proper db query?
-        return result.rows.map(row => row.doc).filter(el => el.safekey === safeKey)[0];
+        return result.rows.map(row => row.doc).filter(el => 'safekey' in el && el.safekey === safeKey)[0];
       });
     },
   });
@@ -93,12 +159,12 @@ window.searchApp = () => {
   async function filterMaterialsByName(searchText) {
     // await new Promise(resolve => setTimeout(resolve, 1000));// TODO Just for testing
     return await Alpine.store('db').getAll().then((result) => {
-      return result.rows.map(row => row.doc).filter(el => el.key.toLowerCase().includes(searchText.toLowerCase()));
+      return result.filter(el => el.key.toLowerCase().includes(searchText.toLowerCase()));
     });
   }
   async function filterMaterialsByDumpText(searchText) {
     return await Alpine.store('db').getAll().then((result) => {
-      return result.rows.map(row => row.doc).filter(el => el.dump.toLowerCase().includes(searchText.toLowerCase()));
+      return result.filter(el => el.dump.toLowerCase().includes(searchText.toLowerCase()));
     });
   }
 
