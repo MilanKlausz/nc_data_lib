72058cc520c4933f806b21946708f55d3bc5a495
MilanKlausz <Milan.Klausz@ess.eu>
2024-05-31 13:30:29 +0200

    added short test for the python query interface #24

---
+++ test-helpers/testQueryInterface.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from queryInterface import performQuery
+
+materialShortKey = 'some.ncmat'
+queryString = "some"
+databasePath = Path(__file__).parent.resolve() / "test_db.json"
+queryResult = performQuery(queryString, databasePath)
+
+materialFound = False
+for result in queryResult:
+  if result['entry']['data']['title'] == materialShortKey:
+    materialFound = True
+
+import sys
+if materialFound:
+    sys.exit(0)  # Exit successfully if material is found
+else:
+    print(f"Error in {Path(__file__)}: Material '{materialShortKey}' not found.", file=sys.stderr)
+    sys.exit(1)  # Exit with an error if material is not found
